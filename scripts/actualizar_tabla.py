b3bfb4c454b85cccaaf2acff393cd0e4ce6c957d
Jpgarciaagp <jpgarciaagp@gmail.com>
2023-11-20 12:44:32 -0500

    Corrección calendario

--- scripts/actualizar_tabla.py
+++ scripts/actualizar_tabla.py
@@ -18,7 +18,7 @@ from databases import Databases
 def main():
     print('Inicializando programa...\n')
     db = Databases()
-    df_base = pd.read_sql(parameters.queries['query_calendario'], db.conns['conn_calenda']).astype({'Orden': int, 'ZFER': int})
+    df_base = pd.read_sql(parameters.queries['query_calendario'], db.conns['conn_calenda']).astype({'Orden': 'int64', 'ZFER': int})
     unique_order = list(df_base['Orden'].unique())
     cal_unique_order = str(unique_order)[1:-1]
     unique_zfer = list(df_base['ZFER'].unique())
@@ -31,7 +31,7 @@ def main():
     print('Leyendo datos desde hojas de ruta de mecanizado...\n')
     # Create a query for the HR table - START
     parameters.create_query(query=f"""WITH a as (SELECT * FROM HR_MATERIALS WHERE ORDEN in ({cal_unique_order}))
-                            SELECT hr.ID_HRUTA, TXT_MECANIZADO, a.MATERIAL as ZFOR FROM ODATA_HR_CONSULTA hr
+                            SELECT hr.ID_HRUTA, TXT_MECANIZADO, a.MATERIAL as ZFER FROM ODATA_HR_CONSULTA hr
                                 inner join a on hr.ID_HRUTA = a.ID_HRUTA
                             WHERE TXT_MECANIZADO is not null and TXT_MECANIZADO <> ''
                             GROUP BY hr.ID_HRUTA, TXT_MECANIZADO, a.MATERIAL""", dict_name='hojasruta')    
@@ -43,7 +43,7 @@ def main():
     print('Leyendo datos desde hojas de ruta de serigrafía...\n')
     # Create a query for the HR table to return the windows with black band - START
     parameters.create_query(query=f"""WITH a as (SELECT * FROM HR_MATERIALS WHERE ORDEN in ({cal_unique_order}))
-                            SELECT hr.ID_HRUTA, TXT_VITRIFICADO, a.MATERIAL as ZFOR FROM ODATA_HR_CONSULTA hr
+                            SELECT hr.ID_HRUTA, TXT_VITRIFICADO, a.MATERIAL as ZFER FROM ODATA_HR_CONSULTA hr
                                 inner join a on hr.ID_HRUTA = a.ID_HRUTA
                             WHERE TXT_MECANIZADO is not null and TXT_MECANIZADO <> ''
                             GROUP BY hr.ID_HRUTA, TXT_VITRIFICADO, a.MATERIAL""", dict_name='hojasruta_serigrafia')
@@ -62,15 +62,12 @@ def main():
     df = pd.merge(df, df_tiemposcnc, on='ZFER', how='left') ## ESTA ESTÁ METIENDO LOS MATERIALES
 
     # Extraer los valores null en el ZFOR
-    df_nullZFOR = df[pd.isnull(df['ZFOR'])] 
-    df = df.dropna(subset=['ZFOR'])
-    df = pd.merge(df.astype({'ZFOR': int}), df_pinturas[['ZFOR', 'ClaveModelo', 'Operacion2']].astype({'ZFOR': int}), 
-                  on=['ZFOR', 'ClaveModelo'], how='left').drop_duplicates()  
+    df = pd.merge(df.astype({'ZFER': int}), df_pinturas[['ZFER', 'ClaveModelo', 'Operacion2']].astype({'ZFER': int}), 
+                  on=['ZFER', 'ClaveModelo'], how='left').drop_duplicates()
     print('Combinando tablas...\n')
     # Combinar los vidrios con mecanizado de df
-    df = pd.merge(df.astype({'ZFOR': int}), df_hojasruta[['ZFOR', 'ClaveModelo', 'Operacion1']].astype({'ZFOR': int}), 
-                  on=['ZFOR', 'ClaveModelo'], how='left').drop_duplicates()
-    df = pd.concat([df, df_nullZFOR]) # Introducir de nuevo los lites sin ZFOR para referencia
+    df = pd.merge(df.astype({'ZFER': int}), df_hojasruta[['ZFER', 'ClaveModelo', 'Operacion1']].astype({'ZFER': int}), 
+                  on=['ZFER', 'ClaveModelo'], how='left').drop_duplicates()
     df = df.fillna({'BordePintura': '', 'BordePaquete': '', 'ClaveModelo':'', 'Operacion1':'', 'Operacion2':'', 'ZFOR': 0, 'Tiempo': 0})    
     
     df = df.reset_index()
@@ -78,7 +75,7 @@ def main():
     df2 = df2.rename({'POSICION': 'Posicion', 'CLASE': 'Material', 'ANCHO': 'Ancho', 'LARGO': 'Largo'}, axis=1)
     df2 = df2.drop_duplicates(subset=['Orden', 'ZFER', 'ClaveModelo'], keep='first')
     df2 = df2.fillna({'BordePintura': '', 'BordePaquete': '', 'ClaveModelo':'', 'Operacion1':'', 'Operacion2':'', 'ZFOR': 0, 'TiempoMecanizado': 0})
-    df2 = df2.astype({'Orden': int, 'ZFER': int, 'ZFOR': int, 'CodTipoPieza': int, 'Material': str, 'Ancho': float,
+    df2 = df2.astype({'Orden': 'int64', 'ZFER': 'int64', 'ZFOR': 'int64', 'CodTipoPieza': int, 'Material': str, 'Ancho': float,
                       'Largo': float, 'ClaveModelo': str, 'Operacion1': str, 'Operacion2': str, 'Perimetro': float, 'TiempoMecanizado': float,
                       'BrilloC': bool, 'BrilloP': bool, 'Bisel': bool, 'CantoC': bool})
     df2.index = df2.index.rename('ID')
